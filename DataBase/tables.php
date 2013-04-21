ceac64fe39d969156205567658cfef4be562f4c0
Ronen Dancziger <ronend@tp-ronend.(none)>
2013-04-22 00:01:16 +0300

    In the middle of refactor. Push to backup

--- DataBase/tables.php
+++ DataBase/tables.php
@@ -1,86 +1,13 @@
 <?php
 class UserTable { 
 	const TAG = 'tables.php, UserTable:'; 
-	const TABLE = 'users';
+	const TABLE_NAME = 'users';
 	const USER_ID_OFFSET = 320000200020001;
-	const USER_NORMAL_STRING_LEN = 50;
+	const STANDARD_STRING_LEN = 50;
 	const USER_TYPES_LIST = 'skhr facebook';
 	
 	final public static function columns() {
-		$columns = array(
-				'user_id' => array(
-						'presented_name' => 'skhr_id',
-						'validate' => 'user_id_validate'
-				),
-				'type' => array(
-						'presented_name' => 'type',
-						'rule' => 'type_rule',
-						'validate' => 'type_validate'
-				),
-				'email' => array(
-						'presented_name' => 'email',
-						'validate' => 'email_validate'
-				),
-				'fb_id' => array(
-						'presented_name' => 'id',
-						'validate' => 'fb_id_validate'
-				),
-				'username' => array(
-						'presented_name' => 'username',
-						'validate' => 'username_validate'
-				),
-				'password' => array(
-						'presented_name' => 'password',
-						'validate' => 'password_validate'
-				),
-				'name' => array(
-						'presented_name' => 'name',
-						'validate' => 'name_validate'
-				),
-				'first_name' => array(
-						'presented_name' => 'first_name',
-						'validate' => 'first_name_validate'
-				),
-				'last_name' => array(
-						'presented_name' => 'last_name',
-						'validate' => 'last_name_validate'
-				),
-				'fb_link' => array(
-						'presented_name' => 'fb_link',
-						'validate' => 'fb_link_validate'
-				),
-				'birthday' => array(
-						'presented_name' => 'birthday',
-						'validate' => 'birthday_validate'
-				),
-				'gender' => array(
-						'presented_name' => 'gender',
-						'rule' => 'gender_rule',
-						'validate' => 'gender_validate'
-				),
-				'timezone' => array(
-						'presented_name' => 'timezone',
-						'validate' => 'timezone_validate'
-				),
-				'locale' => array(
-						'presented_name' => 'locale',
-						'validate' => 'locale_validate'
-				),
-				'verified' => array(
-						'presented_name' => 'verified',
-						'validate' => 'verified_validate',
-						'rule' => 'verified_rule'
-				),
-				'updated_time' => array(
-						'presented_name' => 'updated_time',
-						'validate' => 'updated_time_validate'
-				),
-				'created_time' => array(
-						'presented_name' => 'created_time',
-						'validate' => 'created_time_validate'
-				)
-		);
-		return $columns;
+		return(parse_ini_file('./DataBase/tables/user.ini',true));
 	}
 	
 	// ---------------- RULES:
@@ -138,28 +65,28 @@ class UserTable {
 	
 	final public static function username_validate($field_str) {
 		$sanitize = filter_var($field_str,FILTER_SANITIZE_STRING);
-		if (strlen($sanitize) !== $field_str || empty($field_str) || strlen($field_str) > self::USER_NORMAL_STRING_LEN) {
+		if (strlen($sanitize) !== $field_str || empty($field_str) || strlen($field_str) > self::STANDARD_STRING_LEN) {
 			throw new SKHR_Exception(self::TAG.'Field: username. ', Messages::INVALID_FIELD_VALUE);
 		}
 	}
 	
 	final public static function name_validate($field_str) {
 		$sanitize = filter_var($field_str,FILTER_SANITIZE_STRING);
-		if (strlen($sanitize) !== $field_str || empty($field_str) || strlen($field_str) > self::USER_NORMAL_STRING_LEN) {
+		if (strlen($sanitize) !== $field_str || empty($field_str) || strlen($field_str) > self::STANDARD_STRING_LEN) {
 			throw new SKHR_Exception(self::TAG.'Field: name. ', Messages::INVALID_FIELD_VALUE);
 		}
 	}
 	
 	final public static function first_name_validate($field_str) {
 		$sanitize = filter_var($field_str,FILTER_SANITIZE_STRING);
-		if (strlen($sanitize) !== $field_str || empty($field_str) || strlen($field_str) > self::USER_NORMAL_STRING_LEN) {
+		if (strlen($sanitize) !== $field_str || empty($field_str) || strlen($field_str) > self::STANDARD_STRING_LEN) {
 			throw new SKHR_Exception(self::TAG.'Field: first_name. ', Messages::INVALID_FIELD_VALUE);
 		}
 	}
 	
 	final public static function last_name_validate($field_str) {
 		$sanitize = filter_var($field_str,FILTER_SANITIZE_STRING);
-		if (strlen($sanitize) !== $field_str || strlen($field_str) > self::USER_NORMAL_STRING_LEN) {
+		if (strlen($sanitize) !== $field_str || strlen($field_str) > self::STANDARD_STRING_LEN) {
 			throw new SKHR_Exception(self::TAG.'Field: last_name. ', Messages::INVALID_FIELD_VALUE);
 		}
 	}
@@ -180,7 +107,7 @@ class UserTable {
 	
 	final public static function birthday_validate($field_str) {
 		$sanitize = filter_var($field_str,FILTER_SANITIZE_STRING);
-		if (strlen($field_str) > self::USER_NORMAL_STRING_LEN) {
+		if (strlen($field_str) > self::STANDARD_STRING_LEN) {
 			throw new SKHR_Exception(self::TAG.'Field: birthday. ', Messages::INVALID_FIELD_VALUE);
 		}
 	}
--- welcome_api.php
+++ welcome_api.php
@@ -5,9 +5,12 @@
 // Client side POST:
 // curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d @b.json http://localhost:80/rest/api/user/123/
 
+echo "\n" . print_r(parse_ini_file('./DataBase/tables/user.ini',true)) . "\n";
 require_once("api/rest.php");
 require_once 'api/execute_request.php';
 $rest_request = RestUtils::processRequest();
 $execute_result = new APIExecutor($rest_request);
+
+
 ?>
 
